026058dbfcb1ef8b891ca24055c3363d9e05272e
Mehmet <mehmethayirli0@gmail.com>
2024-04-27 17:06:59 +0300

    c.statements,loops

--- src/main.rs
+++ src/main.rs
@@ -52,8 +52,7 @@ fn main() {
 
     println!("{:?}", vec_arr);
 
-    //structs
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     struct StructName{
         field1: i32,
         field2: f64
@@ -66,19 +65,71 @@ fn main() {
 
     println!("{:?}", struct_var);
 
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
+    enum Department {
+        Computer(
+            Vec<String>),
+        Maths((u16, String))
+    }
+
+    #[derive(Debug, PartialEq)]
     struct Student{
         field1: String,
+        department: Department,
         field2: i32,
         field3: f64
     }
 
-    let struct_var2 = Student {
+    let mut student = Student {
         field1: "Yaşar Güzel".to_string(),
+        department: Department::Computer(vec!("Rust".to_string())),
         field2: 2,
         field3: 99.9,
     };
 
-    println!("{:?}", struct_var2);
+    student.department = Department::Maths((10,"Rust".to_string()));
 
+    println!("{:?}", student);
+
+    enum EnumName {
+        Enum1,
+        Enum2(StructName),
+        Enum3(u32),
+        Enum4((i32,f64,char))
+    }
+
+    // control  statements
+    // if, if - else, 
+    if student.department == Department::Computer(vec!["ICP".to_string(), "Rust".to_string()]){
+        println!("IC Hub");
+    }else {
+        println!("Rust");
+    }
+
+
+    // loops
+    loop{
+        println!("One time execution");
+        break;
+    }
+
+    let mut index = 0;
+    while index < 2 {  
+        println!("{}", index);
+        index += 1;
+    }
+
+    for i in (1..=5).step_by(2){
+        println!("for loop: {}", i);
+    }
+
+    match student.department{
+        Department::Computer(lessons) => {
+        println!("Alınan Dersler: {:?}", lessons);
+        }
+        _ => {
+            println!("Daha sonra dene");
+        }
+    }
+     
 }
